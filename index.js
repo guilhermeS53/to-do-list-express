7d75fa83369309f7fc3670354cbe8de16c149e5f
Guilherme Sousa <guilherme.gs17@outlook.com>
2023-09-24 15:45:33 -0300

    Adição dos componentes do MVC

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+const port = 3000;
+
+// Route
+app.get("/home", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(port, () =>
+  console.log("Servidor rodando em http://localhost:${port}")
+);
